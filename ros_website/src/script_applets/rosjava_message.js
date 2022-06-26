d9444a507ef5646505131ed67b5abb94516c807d
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-06-26 03:11:32 -0400

    progess

---
+++ ros_website/src/script_applets/rosjava_message.js
@@ -0,0 +1,12 @@
+<applet id="app"
+            archive="../../jar_applets/rosjava_messages.jar"
+            code="rosjava_messages" ...>
+    </applet>
+
+    <script language="javascript">
+        // Classes inside rosjava messages
+        
+        // var five = app.methodReturningInt();
+        // var hello = app.methodReturningString();
+        // app.methodReturningObject().anotherMethod();
+    </script>
